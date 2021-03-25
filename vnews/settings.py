5fc11e72ea9bf482f1c27a99baa0bf0cfa9bef27
vivek singh <65790028+vivek147-cyber@users.noreply.github.com>
2021-03-25 20:20:25 +0530

    Update settings.py

--- vnews/settings.py
+++ vnews/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-7re)wp)yp=$l6)8hd2&rgt#l33^@qr@g$9ah56r4oy7%5q#qk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','https://v7news.herokuapp.com/']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
