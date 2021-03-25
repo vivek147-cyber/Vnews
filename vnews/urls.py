ee647aec680b79b280aecacd5bb2c6a71a9a71cf
vivek singh <65790028+vivek147-cyber@users.noreply.github.com>
2021-03-25 20:21:45 +0530

    Update urls.py

--- vnews/urls.py
+++ vnews/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+from django.views.static import serve
+from django.conf.urls import url
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('news/',include('news.urls')),
+    
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
